371aad40c04aeea5ded91cd5953c5846f4b0ae40
naderm1991 <nadermohammad9@gmail.com>
2022-08-12 15:20:21 +0200

    switch to live credentials

--- config.php
+++ config.php
@@ -1,8 +1,17 @@
 <?php
-if (!defined('DB_HOST')) {define("DB_HOST", 'localhost');}
-if (!defined('DB_USERNAME')) {define("DB_USERNAME", 'root');}
-if (!defined('DB_PASSWORD')) {define("DB_PASSWORD", '');}
-if (!defined('DB_NAME')) {define("DB_NAME", 'forms_app');}
+//if (!defined('DB_HOST')) {define("DB_HOST", 'localhost');}
+//if (!defined('DB_USERNAME')) {define("DB_USERNAME", 'root');}
+//if (!defined('DB_PASSWORD')) {define("DB_PASSWORD", '');}
+//if (!defined('DB_NAME')) {define("DB_NAME", 'forms_app');}
+//
+//if (!defined('BASE_URL')) {define("BASE_URL", 'http://localhost/forms-app');}
+//if (!defined('EMAIL')) {define("EMAIL", 'emailtest@YOURDOMAIN');}
 
-if (!defined('BASE_URL')) {define("BASE_URL", 'http://localhost/forms-app');}
+
+if (!defined('DB_HOST')) {define("DB_HOST", 'us-cdbr-east-06.cleardb.net');}
+if (!defined('DB_USERNAME')) {define("DB_USERNAME", 'b45fb0fa3f805a');}
+if (!defined('DB_PASSWORD')) {define("DB_PASSWORD", 'b8a5c625');}
+if (!defined('DB_NAME')) {define("DB_NAME", 'heroku_d3cb62d851fe922');}
+
+if (!defined('BASE_URL')) {define("BASE_URL", 'https://forms-app-com.herokuapp.com/');}
 if (!defined('EMAIL')) {define("EMAIL", 'emailtest@YOURDOMAIN');}
